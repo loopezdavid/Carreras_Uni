b636c14eea272ac03bcc1f1dda8b445a3d3d6c9f
Mario German <arancibigm@gmail.com>
2025-10-01 19:51:31 +0200

    add functions

--- dao_Carrera.py
+++ dao_Carrera.py
@@ -1,29 +1,37 @@
 import mysql.connector
-
-cnx = mysql.connector.connect(
-    host="127.0.0.1",
-    port=3306,
-    user="root",
-    #password="Bootcamp123"
-    password="12345678",
-    database="carreras")
+cnx = None
+def connect_db(cnx):
+    cnx = mysql.connector.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        #password="Bootcamp123"
+        password="12345678",
+        database="carreras")
+    return cnx
 
 # Get a cursor
 cur = cnx.cursor()
-query = "SELECT * from carrera"
-query2 = "insert into alumno (nombre, apellido) values ('Juan', 'Perez')"
-query3 = "delete from alumno where nombre='Tomas' and apellido='Castillo'"
-query4 = "DELETE FROM `uni`.`curso` WHERE (`Id_Alumno` = '39') and (`Id_Materia` = '11'); DELETE FROM `uni`.`nota` WHERE (`Id_Nota` = '39'); DELETE FROM `uni`.`alumno` WHERE (`Id_Alumno` = '39');"
-query5 = "update alumno set nombre='Fernanada', apellido='Castillo' where Id_Alumno=39"
-
-
-
-cur.execute(query)
-
-for (nombre) in cur:
-    print(nombre)
-print(f"alumnos totales: {cur.rowcount}")
-
+def añadir_carrea(nombre_carrera):
+    try:
+        cur.execute("insert into carrera (Nombre_Carrera) values ('nombre_carrera')")
+    except:
+        print("Error al insertar la carrera")
+def modificar_carrera(nombre_carrera,id_carrera):
+    try:
+        cur.execute("update carrera set Nombre_Carrera='{nombre_carrera}', where id_Carrera={id_carrera}")
+    except:
+        print("Error al modificar la carrera")  
+def ver_carreras():
+    try:
+        cur.execute("select * from carrera")
+    except:
+        print("Error al ver las carreras")
+def borrar_carrera(id_carrera):
+    try:
+        cur.execute("delete from carrera where id_Carrera={id_carrera}")
+    except:
+        print("Error al borrar la carrera")  
 
 cur.close()
 cnx.close()
