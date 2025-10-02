cba6b8bac3445740e56d09a345d1181162d22c7c
Mario German <arancibigm@gmail.com>
2025-10-02 21:48:31 +0200

    agregar el %s para mayor seguridad

--- dao_Carrera.py
+++ dao_Carrera.py
@@ -10,6 +10,8 @@ def connect_db():
         database="carreras")
     return cnx
 
+
+
 # Get a cursor
 def user_query(cur,query):
     try:
@@ -18,33 +20,36 @@ def user_query(cur,query):
     except mysql.connector.Error as err:
         print(f"Error: {err}")
         return None    
+####.execute("INSERT INTO table VALUES(%s,%s)", (int(id), string))
 
 def añadir_carrea(cur,nombre_carrera):
     try:
-        cur.execute(f"insert into carrera (`Nombre_Carrera`) values ('{nombre_carrera}')")
+        cur.execute("insert into carrera (`Nombre_Carrera`) values (%s)", (nombre_carrera,))
     except:
         print("Error al insertar la carrera")
         
 def modificar_carrera(cursor,nombre_carrera,id_carrera):
     try:
-        cursor.execute(f"select Nombre_Carrera from carrera where id_Carrera={id_carrera}")
+        cursor.execute("select Nombre_Carrera from carrera where id_Carrera = %s", (id_carrera,))
         resultados = cursor.fetchall()
-        cursor.execute(f"update carrera set Nombre_Carrera='{nombre_carrera}' where id_Carrera={id_carrera}")
+        cursor.execute("update carrera set Nombre_Carrera= %s where id_Carrera = %s", (nombre_carrera,id_carrera,))
         return resultados
     except:
         print("Error al modificar la carrera") 
          
 def ver_carreras(cur):
     try:
-       return cur.execute(f"select * from carrera")
+        cur.execute("select * from carrera")
+        return cur.fetchall()
+        
     except:
         print("Error al ver las carreras")
         
 def borrar_carrera(cursor,id_carrera):
     try:         
-        cursor.execute(f"select Nombre_Carrera from carrera where id_Carrera={id_carrera}")
+        cursor.execute("select Nombre_Carrera from carrera where id_Carrera = %s", (id_carrera,))
         resultados = cursor.fetchall()
-        cursor.execute(f"delete from carrera where id_Carrera={id_carrera}")
+        cursor.execute("delete from carrera where id_Carrera = %s", (id_carrera,))
         return resultados
     except:
         print("Error al borrar la carrera")  
