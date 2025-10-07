fd25c9b7197491a8f6a26293e30c8997e284b8cd
Mario German <arancibigm@gmail.com>
2025-10-07 20:27:17 +0200

    change print lista carreras

--- env_act_uni/main.py
+++ env_act_uni/main.py
@@ -5,6 +5,7 @@ import mysql.connector
 from tabulate import tabulate
 carreras=[]
 data=[]
+tabla = None
 start_program = False
 # Código para activar la negrita
 NEGRITA = '\033[1m'
@@ -57,7 +58,7 @@ def añadir_carrera(cursor):
         print(f"\nla carrera {NEGRITA}{nombre}{RESET} se ha añadido.\n")
     pausa()
 
-def actualizar_carrera(cursor):
+def actualizar_carrera(cursor,tabla ):
     tabla, carreras= ver_carreras(cursor)
     print(tabla)
     print("seleccione una opcion.. ")
@@ -83,13 +84,14 @@ def ver_carreras(cursor):
     if not resultados:
         print("No hay carreras registradas.")
     else:
-        print("\n--- LISTA DE CARRERAS ---")
         for (id_Carrera,Nombre_Carrera,nota_corte,duracion) in resultados:
             carrera_temp = c.carrera(Nombre_Carrera,id_Carrera,duracion,nota_corte)
+          
             id_carrera = carrera_temp.get_id_carrera()
             nombre_carrera = carrera_temp.get_nombre_carrera()
             nota_de_corte = carrera_temp.get_nota_corte()
             duracion_carrera = carrera_temp.get_duracion()
+           
             data.append([id_carrera,nombre_carrera,nota_de_corte,duracion_carrera])
             carreras.append(carrera_temp)
             
@@ -105,7 +107,8 @@ def ver_carreras(cursor):
     )
     return table, carreras
 
-def borrar_carrera(cursor):
+def borrar_carrera(cursor,table):
+    print(table)
     id_carrera = input("Introduce el ID de la carrera a borrar: ")
     borrar_carrera = c.carrera("",id_carrera)
     resultados = dao.borrar_carrera(cursor,borrar_carrera.getter_id())
@@ -128,27 +131,30 @@ def user_query(cursor):
             print(row)
     pausa()
 # Programa principal
-while start_program:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    mostrar_menu()
-    opcion = input("Selecciona una opción: ")
-
-    if opcion == "1":
-        añadir_carrera(cursor)
-    elif opcion == "2":
-        actualizar_carrera(cursor)
-    elif opcion == "3":
+def main(start_program):
+    while start_program:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        mostrar_menu()
+        opcion = input("Selecciona una opción: ")
         tabla, carreras = ver_carreras(cursor)
-        print(tabla)    
-        pausa()
-    elif opcion == "4":
-        borrar_carrera(cursor)
-    elif opcion == "5":
-        cursor.close()
-        conexion.close()
-        print("\nSaliendo del programa...\n")
-        start_program = False
-    elif opcion == "0":
-        user_query(cursor)
-    else:
-        print("Opción no válida.")
+        if opcion == "1":
+            añadir_carrera(cursor)
+        elif opcion == "2":
+            actualizar_carrera(cursor,tabla)
+        elif opcion == "3":
+            print("\n--- LISTA DE CARRERAS ---")
+            tabla, carreras = ver_carreras(cursor)
+            print(tabla)    
+            pausa()
+        elif opcion == "4":
+            borrar_carrera(cursor,tabla)
+        elif opcion == "5":
+            cursor.close()
+            conexion.close()
+            print("\nSaliendo del programa...\n")
+            start_program = False
+        elif opcion == "0":
+            user_query(cursor)
+        else:
+            print("Opción no válida.")
+main(start_program)
